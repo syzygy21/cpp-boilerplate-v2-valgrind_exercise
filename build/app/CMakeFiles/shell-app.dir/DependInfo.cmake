21440ea7010b74dc2ec3bc80959e8476b816faac
Navdeep Singh <singh.nav2100@gmail.com>
2024-10-16 23:41:38 -0400

    Add files via upload

---
+++ build/app/CMakeFiles/shell-app.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/navdeep/cpp-boilerplate-v2-valgrind_exercise/app/AnalogSensor.cpp" "app/CMakeFiles/shell-app.dir/AnalogSensor.cpp.o" "gcc" "app/CMakeFiles/shell-app.dir/AnalogSensor.cpp.o.d"
+  "/home/navdeep/cpp-boilerplate-v2-valgrind_exercise/app/main.cpp" "app/CMakeFiles/shell-app.dir/main.cpp.o" "gcc" "app/CMakeFiles/shell-app.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/app/CMakeFiles/shell-app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell-app.dir/AnalogSensor.cpp.o"
+  "CMakeFiles/shell-app.dir/AnalogSensor.cpp.o.d"
+  "CMakeFiles/shell-app.dir/main.cpp.o"
+  "CMakeFiles/shell-app.dir/main.cpp.o.d"
+  "shell-app"
+  "shell-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shell-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
